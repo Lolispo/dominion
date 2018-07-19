5aa1710faf80a36b74106ae8dc17c40eaa346210
Petter Andersson <petterandersson95@gmail.com>
2018-07-20 00:21:11 +0200

    Moved html_css code to new file, added shop. Prep for func

--- gameScript.js
+++ gameScript.js
@@ -8,6 +8,10 @@ var turn = 0;
 
 /*
 	TODO
+	Make Player own areas more clearer, border or something
+	Shop
+		Show affordable cards
+		Show selected card
 	Scalable card sizes
 	sortable hand (instant and on command, when new cards are added)
 	Dropdowns of cards for buying, 4 different - All and the 3 categories
@@ -48,19 +52,6 @@ async function initCardsGlobal(){
 	initCards();
 	console.log('Cards should be done - Shop Init');
 	initShopHTML();
-}
-
-function initShopHTML(){
-	var cards = cards_global;
-	initNewUIElement('ul', new Map().set('id', 'mainShop'), 'shop');
-	cards.forEach(function(value, key){
-		console.log(key + ' = ' + value);
-		initNewUIElement('li', new Map().set('id', value.name), 'mainShop').innerHTML = value.name;				
-	});
-}
-
-function updateTurnUI(){
-	document.getElementById('turn').innerHTML = 'Player ' + (turn+1) + ':s turn';
 }
 
 function changeTurn(){
@@ -117,78 +108,8 @@ function isTurn(playerIndex){
 	return false;
 }
 
-// HTML Stuff
 
-function initNewUIElement(typeEl, properties = new Map(), parentID, cssClass = ''){
-	var div = document.getElementById(parentID);
-	var el = document.createElement(typeEl);
-	properties.forEach(function(value, key) {
-		el.setAttribute(key, value);
-	});
-	addCSSClassEl(el, cssClass);
-	div.appendChild(el);
-	return el;
-}
 
-function createButton(text, parentID, id, callback, cssClass){
-	//console.log('DEBUG @createButton');
-	var el = document.getElementById(parentID);
-	var button = document.createElement('button');
-	button.type = 'button';
-	button.innerHTML = text;
-	button.id = id;
-	el.appendChild(button);
-	button.addEventListener('click', function(){
-		callback();
-	});
-	addCSSClassEl(button, cssClass);
-}
-
-function deleteButton(id, parentID){
-	var handEl = document.getElementById(parentID); // Remove the button from parentID with id = id
-	handEl.removeChild(document.getElementById(id));
-}
-
-function removeChildren(id){
-	var el = document.getElementById(id);
-	for(var i = el.childNodes.length-1; i >= 0; i--){
-		el.removeChild(el.childNodes[i]);
-	}
-}
-
-function addCSSClassID(id, cssClass){
-	var el = document.getElementById(id);
-	addCSSClassEl(el, cssClass);
-}
-
-function removeCSSClassID(id, cssClass){
-	var el = document.getElementById(id);
-	removeCSSClassEl(el, cssClass);
-}
-
-function addCSSClassEl(el, cssClass){
-	if(Array.isArray(cssClass)){
-		for(var i = 0; i < cssClass.length; i++){
-			el.classList.add(cssClass[i]);
-		}
-	}else if(cssClass !== ''){
-		el.classList.add(cssClass);		
-	}
-}
-
-function removeCSSClassEl(el, cssClass){
-	if(Array.isArray(cssClass)){
-		for(var i = 0; i < cssClass.length; i++){
-			el.classList.remove(cssClass[i]);
-		}
-	}else if(cssClass !== ''){
-		el.classList.remove(cssClass);		
-	}
-}
-
-function changeButtonText(id, text){
-	document.getElementById(id).innerHTML = text;
-}
 
 function getPlayer(pid){
 	return players[pid];
---
+++ html_css_functions.js
@@ -0,0 +1,117 @@
+// Author Petter Andersson
+'use strict'
+
+// HTML Stuff
+
+function initNewUIElement(typeEl, properties = new Map(), parentID, cssClass = ''){
+	var div = document.getElementById(parentID);
+	var el = document.createElement(typeEl);
+	properties.forEach(function(value, key) {
+		el.setAttribute(key, value);
+	});
+	addCSSClassEl(el, cssClass);
+	div.appendChild(el);
+	return el;
+}
+
+function createButton(text, parentID, id, callback, cssClass){
+	//console.log('DEBUG @createButton');
+	var el = document.getElementById(parentID);
+	var button = document.createElement('button');
+	button.type = 'button';
+	button.innerHTML = text;
+	button.id = id;
+	el.appendChild(button);
+	button.addEventListener('click', function(){
+		callback();
+	});
+	addCSSClassEl(button, cssClass);
+}
+
+function deleteButton(id, parentID){
+	var handEl = document.getElementById(parentID); // Remove the button from parentID with id = id
+	handEl.removeChild(document.getElementById(id));
+}
+
+function removeChildren(id){
+	var el = document.getElementById(id);
+	for(var i = el.childNodes.length-1; i >= 0; i--){
+		el.removeChild(el.childNodes[i]);
+	}
+}
+
+// Make all of these into 2 methods with args, taking add, remove toggle. TODO
+
+function addCSSClassID(id, cssClass){
+	var el = document.getElementById(id);
+	addCSSClassEl(el, cssClass);
+}
+
+function toggleCSSClassID(id, cssClass){
+	var el = document.getElementById(id);
+	toggleCSSClassEl(el, cssClass);
+}
+
+function removeCSSClassID(id, cssClass){
+	var el = document.getElementById(id);
+	removeCSSClassEl(el, cssClass);
+}
+
+function addCSSClassEl(el, cssClass){
+	if(Array.isArray(cssClass)){
+		for(var i = 0; i < cssClass.length; i++){
+			el.classList.add(cssClass[i]);
+		}
+	}else if(cssClass !== ''){
+		el.classList.add(cssClass);		
+	}
+}
+
+function removeCSSClassEl(el, cssClass){
+	if(Array.isArray(cssClass)){
+		for(var i = 0; i < cssClass.length; i++){
+			el.classList.remove(cssClass[i]);
+		}
+	}else if(cssClass !== ''){
+		el.classList.remove(cssClass);		
+	}
+}
+
+function toggleCSSClassEl(el, cssClass){
+	if(Array.isArray(cssClass)){
+		for(var i = 0; i < cssClass.length; i++){
+			el.classList.toggle(cssClass[i]);
+		}
+	}else if(cssClass !== ''){
+		el.classList.toggle(cssClass);		
+	}
+}
+
+function initShopHTML(){
+	var cards = cards_global;
+	initNewUIElement('div', new Map().set('id', 'mainShop'), 'shop').innerHTML = 'Shop';
+	initNewUIElement('div', new Map().set('id', 'shopCards'), 'mainShop');
+	cards.forEach(function(value, key){
+		var properties = new Map();
+		properties.set('id', 'card_' + value.id);
+		properties.set('src', getCorrectImage(value));
+		console.log(key + ' = ' + value);
+		var el = initNewUIElement('img', properties, 'shopCards', ['card_smaller', getCssClassCard(value)]);
+		el.addEventListener('click', function(res){
+			// TODO: 
+		});
+	});
+	createButton('Toggle Shop', 'mainShop', 'showShop', (function(){ // Change to Open and Close / Change on toggle TODO
+		// Show / dont show shop
+		toggleCSSClassID('shopCards', 'invis');
+	}).bind(this));	
+}
+
+function changeButtonText(id, text){
+	document.getElementById(id).innerHTML = text;
+}
+
+
+function updateTurnUI(){
+	document.getElementById('turn').innerHTML = 'Player ' + (turn+1) + ':s turn';
+}
